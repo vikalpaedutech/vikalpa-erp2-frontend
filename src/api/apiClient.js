2951ced3863aaf22f910fbb5364692f920374af2
iamshubhamshah <mb.shbuhamshah@gmail.com>
2026-09-23 18:07:56 +0530

    updates

--- src/api/apiClient.js
+++ src/api/apiClient.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const apiClient = axios.create({
-  baseURL: "http://localhost:8000/api/v1",
+  baseURL: "http://localhost:8100/api/v1",
   withCredentials: true,
   headers: {
     "Content-Type": "application/json",
